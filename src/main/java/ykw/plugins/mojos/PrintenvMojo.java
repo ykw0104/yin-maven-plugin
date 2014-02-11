83684de62568a049af9ad8100afa9df99c2b0a0a
ykw0104 <ykw0104@163.com>
2014-02-11 13:32:41 +0800

    Signed-off-by: ykw0104 <ykw0104@163.com>

--- src/main/java/ykw/plugins/mojos/PrintenvMojo.java
+++ src/main/java/ykw/plugins/mojos/PrintenvMojo.java
@@ -17,6 +17,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.w3c.dom.Document;
